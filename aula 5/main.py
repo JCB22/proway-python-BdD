1a8dc14ded673e232fd2bda0c1b5cb960083f1e2
JCB22 <joaovaltrig@gmail.com>
2022-09-03 15:36:48 -0300

    Aula 5

---
+++ aula 5/main.py
@@ -0,0 +1,9 @@
+from prog01 import inverte_texto
+from exercicios import ex01
+
+if __name__ == '__main__':
+    texto = "Olá Mundo!"
+    textonovo = inverte_texto(texto)
+    print(textonovo)
+
+    print(ex01("AraRa"))
